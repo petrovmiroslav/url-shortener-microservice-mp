467601fe8d76a426692f32f402075fe5ae554067
petrovmiroslav <46135319+petrovmiroslav@users.noreply.github.com>
2019-03-01 12:45:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 'use strict';
 
 var express = require('express');
+var app = express();
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var dns = require('dns');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
-var cors = require('cors');
-
-var app = express();
-
-// Basic Configuration 
 var port = process.env.PORT || 3000;
-
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
-
-app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -25,11 +16,72 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+var Schema = mongoose.Schema;
+
+  var URLShortenerSchema = new Schema({
+    original_url: {
+            type: String,
+            required: true
+          },
+    short_url: String
+  });
+var UrlBase = mongoose.model('UrlBase', URLShortenerSchema);
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+/*mongoose.connection.once('open', function() {
+  console.log("CONNECT");
+});*/
+function addAndSaveUrl(hostname, short_url, res) {
+  var urlToAdd = new UrlBase ({ original_url: hostname, short_url:  short_url});
+    urlToAdd.save(function (err, data) {
+    if (err) return console.error(err);
+    res.json({"original_url": hostname,"short_url": short_url}); 
+  });
+};
+function URLShortener() {
+  return (Date.now()+"mp");
+};
+
+function handlerPOST(req, res) {
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+  const matchUrl = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/.exec(req.body.url);
+  if(matchUrl === null) {
+    return res.json({"error":"invalid URL"});
+  }
+  const url = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/.exec(req.body.url)[0];
+  const hostname = /[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/.exec(url)[0];
+  
+  dns.lookup(hostname, (err, address, family) => {
+    if(address === undefined) {
+       res.json({"error":"invalid URL"});
+    } else {
+       UrlBase.findOne({"original_url": hostname }, function (err, data) {
+          if (err) return console.error(err);
+          if(!data) {
+            const shortUrl = URLShortener();
+            addAndSaveUrl(hostname, shortUrl, res);
+          }
+          if(data) {
+            res.json({"original_url": data.original_url,"short_url": data.short_url});
+          }
+       });
+    }
+  });
+};
+function handlerGET(req, res) {
+  const shortUrl = req.params.url;
+  UrlBase.findOne({"short_url": shortUrl }, function (err, data) {
+          if (err) return console.error(err);
+          if(!data) {
+            res.json({"error": "URL NOT FOUND"});
+          }
+          if(data) {
+            res.redirect("http://"+data.original_url);
+          }   
+  });
+};
+app.route("/api/shorturl/:url").get(handlerGET);
+app.route("/api/shorturl/new").post(handlerPOST);
 
 
 app.listen(port, function () {
